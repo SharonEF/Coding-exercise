918ddfc1ff42a624720f7fdca57954d1c7d3efe0
Sharon Elizabeth Freundlich <elizabeth.freundlich@gmail.com>
2022-10-25 13:55:46 +0300

    Define JS function bingo

--- practice.js
+++ practice.js
@@ -1,6 +1,13 @@
 /*let myStringConcat = 'Pracise';
-console.log('It is good to do coding ' + myCoding + '.');*/
+console.log('It is good to do coding ' + myCoding + '.');
 
 const myStringInterpolate = 'Interpolate';
-console.log(`I am using backticks because I am doing string ${myStringInterpolate}.`);
+console.log(`I am using backticks because I am doing string ${myStringInterpolate}.`);*/
+
+let numFuction = 7;
+if (numFuction > 5) {
+    console.log('Bingo!');
+} else {
+    console.log('Better luck next time!');
+}
 
