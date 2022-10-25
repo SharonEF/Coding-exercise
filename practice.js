6f8303459cc11c87b08499e6571d4b883883b0c9
Sharon Elizabeth Freundlich <elizabeth.freundlich@gmail.com>
2022-10-25 13:39:10 +0300

    Define JS function returning a string

---
+++ practice.js
@@ -0,0 +1,6 @@
+/*let myStringConcat = 'Pracise';
+console.log('It is good to do coding ' + myCoding + '.');*/
+
+const myStringInterpolate = 'Interpolate';
+console.log(`I am using backticks because I am doing string ${myStringInterpolate}.`);
+
