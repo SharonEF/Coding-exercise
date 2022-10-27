e2fd2ae7b55efb056d891bce17e9c7bfc48550ce
Sharon Elizabeth Freundlich <elizabeth.freundlich@gmail.com>
2022-10-27 15:28:59 +0300

    modified Function

--- practice.js
+++ practice.js
@@ -4,10 +4,13 @@ console.log('It is good to do coding ' + myCoding + '.');
 const myStringInterpolate = 'Interpolate';
 console.log(`I am using backticks because I am doing string ${myStringInterpolate}.`);*/
 
-let numFuction = 7;
-if (numFuction > 5) {
-    console.log('Bingo!');
+let number = 7;
+function bingoFun(num) {
+if (num > 5) {
+    return 'Bingo!';
 } else {
-    console.log('Better luck next time!');
+    return 'Better luck next time!';
 }
-
+}
+bingoFun(9);
+console.log(bingoFun(9));
